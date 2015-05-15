a62b24f8ab6011882e842723e18e046e75a79b69
Kevin Ngo <ngoke@mozilla.com>
2015-05-15 14:53:35 -0700

    fancy landing graphic

--- client/js/app.js
+++ client/js/app.js
@@ -35,9 +35,39 @@ var App = React.createClass({
 
 
 var Landing = React.createClass({
+    getInitialState: function() {
+        return {
+            graphicLetterGroups: [
+                'I AM'.split(''),
+                'SHER'.split(''),
+                'LOCKED'.split('')
+            ]
+        };
+    },
+    renderGraphicLetter: function(letter, i) {
+        var graphicLetterClasses = classnames({
+            'landing-graphic-letter': true,
+            'landing-graphic-space': letter === ' '
+        });
+        return <h1 className={graphicLetterClasses}
+                   data-graphic-letter-index={i}>
+          <span>{letter}</span>
+        </h1>
+    },
+    renderGraphicLetterGroup: function(letters, i) {
+        return <div className="landing-graphic-letter-group"
+                    data-graphic-letter-group-index={i}>
+          {letters.map(this.renderGraphicLetter)}
+        </div>
+    },
     render: function() {
         return <div className="landing">
-          <img className="landing-img" src="http://imgur.com/b5jQjd7.png"/>
+          <div className="landing-graphic">
+            <div className="landing-graphic-letters">
+              {this.state.graphicLetterGroups.map(
+               this.renderGraphicLetterGroup)}
+            </div>
+          </div>
           <div className="landing-info">
             <h2>I am Sherlocked, the world's greatest visual regression testing service.</h2>
             <h3>And it is my business to know what other people don't know.</h3>
